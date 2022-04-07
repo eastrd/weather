9e71431bbb8b842e93fcecc55a7a14166ccff9f0
eastrd <e0t3rx@gmail.com>
2022-04-07 21:30:24 +1000

    Add proper formatting

--- weather.lisp
+++ weather.lisp
@@ -1,20 +1,37 @@
 (load "")
 (ql:quickload "drakma")
 (ql:quickload "cl-json")
+(ql:quickload "alexandria")
 
 (defconstant *api-key* 
   "")
 
-(defun make-url (lat lon api-key)
-  (format nil "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=~a&lon=~a&appid=~a" lat lon api-key))
+(defun make-url (&key lat lon api-key)
+  (format nil 
+    "https://api.openweathermap.org/data/2.5/weather?lat=~a&lon=~a&appid=~a" 
+    lat lon api-key))
 
 (defun get-weather (url)
   (let* ((resp (drakma:http-request url))
         (str (byte-to-string resp)))
       (with-input-from-string (s str)
-        (json:decode-json s))))
+        (alexandria:alist-hash-table
+          (json:decode-json s)))))
 
 (defun byte-to-string (b)
   (map 'string #'code-char b))
 
-(get-weather (make-url -37.8863 145.058725 *api-key*))
+(defun k-to-c (k) (- k 273.15))
+
+(defun weather-broadcast (table)
+  (let* ((summary (gethash :weather table))
+        (detail (cdr (third (first summary))))
+        (surburb (gethash :name table))
+        (temps (gethash :main table))
+        (temp-high (cdr (third temps)))
+        (temp-low (cdr (fourth temps))))
+    (format t "Weather at ~a is ~aC-~aC, overall ~a" surburb (k-to-c temp-low) (k-to-c temp-high) detail)))
+
+(get-weather (make-url :lat -37.8863 :lon 145.058725 :api-key *api-key*))
+
+
